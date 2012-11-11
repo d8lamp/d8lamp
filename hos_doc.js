cead392f894abb1b3e03c1f1faf6766ac0f16c0e
Conor O'Gorman <i@conorogorman.net>
2012-11-11 14:27:31 +0000

    simple layers

---
+++ hos_doc.js
@@ -0,0 +1,493 @@
+var data = {
+  "version": 0.6,
+  "generator": "Overpass API",
+  "osm3s": {
+    "timestamp_osm_base": "2012-11-08T23:32:02Z",
+    "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL."
+  },
+  "elements": [
+
+{
+  "type": "node",
+  "id": 262347572,
+  "lat": 53.3327187,
+  "lon": -6.2559869,
+  "tags": {
+    "amenity": "hospital",
+    "name": "Royal Victoria Eye and Ear Hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 264213513,
+  "lat": 53.3446382,
+  "lon": -6.2917191,
+  "tags": {
+    "amenity": "hospital",
+    "created_by": "JOSM",
+    "name": "Saint Patrick's Hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 293526276,
+  "lat": 53.2744435,
+  "lon": -6.3453704,
+  "tags": {
+    "amenity": "doctors"
+  }
+},
+{
+  "type": "node",
+  "id": 298545591,
+  "lat": 53.3848560,
+  "lon": -6.2502463,
+  "tags": {
+    "amenity": "hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 535922186,
+  "lat": 53.2991355,
+  "lon": -6.1783241,
+  "tags": {
+    "amenity": "hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 564399692,
+  "lat": 53.3284650,
+  "lon": -6.2587520,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Archview Physiotherapy"
+  }
+},
+{
+  "type": "node",
+  "id": 564908554,
+  "lat": 53.3259440,
+  "lon": -6.2606277,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Doctor Niall M. Joyce"
+  }
+},
+{
+  "type": "node",
+  "id": 565719710,
+  "lat": 53.3215164,
+  "lon": -6.2543189,
+  "tags": {
+    "addr:city": "Dublin 6",
+    "addr:country": "IE",
+    "addr:housenumber": "9",
+    "addr:street": "Beechwood Avenue Lower",
+    "amenity": "doctors"
+  }
+},
+{
+  "type": "node",
+  "id": 567713114,
+  "lat": 53.3440599,
+  "lon": -6.2652297,
+  "tags": {
+    "addr:city": "Dublin",
+    "addr:country": "IE",
+    "addr:housenumber": "16",
+    "addr:street": "Dame Street",
+    "amenity": "doctors",
+    "name": "Dame Street Medical Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 576104931,
+  "lat": 53.3364283,
+  "lon": -6.2489293,
+  "tags": {
+    "amenity": "doctors",
+    "name": "D2 Medical",
+    "website": "http://www.d2medical.ie/"
+  }
+},
+{
+  "type": "node",
+  "id": 641802162,
+  "lat": 53.3422426,
+  "lon": -6.2518888,
+  "tags": {
+    "amenity": "hospital",
+    "name": "Dublin Dental Hospital",
+    "wheelchair": "yes"
+  }
+},
+{
+  "type": "node",
+  "id": 653874503,
+  "lat": 53.2919147,
+  "lon": -6.3782058,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "yes",
+    "name": "Emergency Department"
+  }
+},
+{
+  "type": "node",
+  "id": 660629039,
+  "lat": 53.3671913,
+  "lon": -6.2728622,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Glasnevin Family Practice",
+    "note": "Medical Clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 681471269,
+  "lat": 53.3510736,
+  "lon": -6.3685932,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Manor Road Surgery"
+  }
+},
+{
+  "type": "node",
+  "id": 700368720,
+  "lat": 53.2860627,
+  "lon": -6.2360763,
+  "tags": {
+    "amenity": "doctors",
+    "name": "VHI Swiftcare Clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 768516996,
+  "lat": 53.3500722,
+  "lon": -6.2672448,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Jervis Medical Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 844874464,
+  "lat": 53.3515440,
+  "lon": -6.3692438,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Health Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 848716763,
+  "lat": 53.3510318,
+  "lon": -6.3789028,
+  "tags": {
+    "amenity": "doctors",
+    "name": "O'Hanlon & O'Neill"
+  }
+},
+{
+  "type": "node",
+  "id": 914627183,
+  "lat": 53.3393686,
+  "lon": -6.2913048,
+  "tags": {
+    "amenity": "hospital",
+    "name": "National Blood Centre,"
+  }
+},
+{
+  "type": "node",
+  "id": 915434098,
+  "lat": 53.3449517,
+  "lon": -6.2498490,
+  "tags": {
+    "amenity": "doctors",
+    "medical": "general",
+    "name": "HSE Primary Care Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 943330362,
+  "lat": 53.3188096,
+  "lon": -6.2791902,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Harold's Cross Medical Practice",
+    "website": "http://www.haroldscrossmedical.ie"
+  }
+},
+{
+  "type": "node",
+  "id": 1031046046,
+  "lat": 53.3671083,
+  "lon": -6.2020454,
+  "tags": {
+    "amenity": "hospital",
+    "wheelchair": "yes"
+  }
+},
+{
+  "type": "node",
+  "id": 1031046047,
+  "lat": 53.3678509,
+  "lon": -6.1990628,
+  "tags": {
+    "amenity": "hospital",
+    "building": "yes",
+    "name": "Irish Wheelchair Association and Respite Centre",
+    "wheelchair": "yes"
+  }
+},
+{
+  "type": "node",
+  "id": 1068058189,
+  "lat": 53.3389619,
+  "lon": -6.2361601,
+  "tags": {
+    "addr:city": "Dublin",
+    "addr:country": "IE",
+    "addr:housename": "Gasworks House",
+    "addr:postal_district": "Dublin 4",
+    "addr:street": "Barrow Street",
+    "amenity": "doctors",
+    "name": "Barrow Medical",
+    "url": "http://www.barrowmedical.ie/"
+  }
+},
+{
+  "type": "node",
+  "id": 1074315483,
+  "lat": 53.2620729,
+  "lon": -6.1617096,
+  "tags": {
+    "amenity": "hospital",
+    "name": "Park Clinic",
+    "source": "local_knowledge"
+  }
+},
+{
+  "type": "node",
+  "id": 1121355068,
+  "lat": 53.2719442,
+  "lon": -6.3058245,
+  "tags": {
+    "addr:street": "Stocking Lane",
+    "amenity": "hospital",
+    "name": "Bloomfield Care Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 1256510141,
+  "lat": 53.3395475,
+  "lon": -6.2949518,
+  "tags": {
+    "amenity": "hospital",
+    "name": "Saint James's Hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 1330821116,
+  "lat": 53.2860885,
+  "lon": -6.2464596,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "no",
+    "name": "Simpson’s Hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 1403890046,
+  "lat": 53.3314490,
+  "lon": -6.2838860,
+  "tags": {
+    "addr:housenumber": "203",
+    "addr:street": "South Circular Road",
+    "amenity": "doctors",
+    "name": "Poly Clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 1409474154,
+  "lat": 53.3399070,
+  "lon": -6.2783710,
+  "tags": {
+    "addr:housenumber": "52",
+    "addr:street": "The Coombe",
+    "amenity": "doctors",
+    "name": "Dr O'Neill"
+  }
+},
+{
+  "type": "node",
+  "id": 1423539583,
+  "lat": 53.3612774,
+  "lon": -6.2850947,
+  "tags": {
+    "addr:city": "Dublin",
+    "addr:country": "IE",
+    "addr:housenumber": "109",
+    "addr:street": "Cabra Road",
+    "amenity": "doctors",
+    "name": "Cabra Medical Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 1426100398,
+  "lat": 53.3307340,
+  "lon": -6.2612437,
+  "tags": {
+    "amenity": "hospital",
+    "name": "Charlemont Clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 1428845616,
+  "lat": 53.3353570,
+  "lon": -6.2945440,
+  "tags": {
+    "addr:housenumber": "478",
+    "addr:street": "South Circular Road",
+    "amenity": "doctors",
+    "name": "Rialto Medical Centre",
+    "phone": "+35314532147",
+    "website": "www.rialtomedical.ie"
+  }
+},
+{
+  "type": "node",
+  "id": 1430846983,
+  "lat": 53.3398027,
+  "lon": -6.2463209,
+  "tags": {
+    "amenity": "hospital",
+    "name": "The National Maternity Hospital",
+    "website": "http://www.nmh.ie/"
+  }
+},
+{
+  "type": "node",
+  "id": 1430922962,
+  "lat": 53.3815153,
+  "lon": -6.2596772,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "no",
+    "name": "Hampstead Private Hospital"
+  }
+},
+{
+  "type": "node",
+  "id": 1496743878,
+  "lat": 53.4036707,
+  "lon": -6.2596173,
+  "tags": {
+    "amenity": "doctors"
+  }
+},
+{
+  "type": "node",
+  "id": 1500501205,
+  "lat": 53.3163742,
+  "lon": -6.3167345,
+  "tags": {
+    "addr:street": "Kimmage Road West",
+    "amenity": "doctors",
+    "name": "Kimmage Medical Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 1529939349,
+  "lat": 53.3956000,
+  "lon": -6.2162165,
+  "tags": {
+    "amenity": "doctors",
+    "name": "Coolock Health Centre"
+  }
+},
+{
+  "type": "node",
+  "id": 1752444661,
+  "lat": 53.2860265,
+  "lon": -6.2362694,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "yes",
+    "name": "VHI switfcare clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 1785369938,
+  "lat": 53.3252884,
+  "lon": -6.2309110,
+  "tags": {
+    "amenity": "hospital",
+    "name": "IEC 2012 Medical Point"
+  }
+},
+{
+  "type": "node",
+  "id": 1785369939,
+  "lat": 53.3272539,
+  "lon": -6.2279511,
+  "tags": {
+    "amenity": "hospital",
+    "name": "IEC 2012 Medical Point"
+  }
+},
+{
+  "type": "node",
+  "id": 1795722943,
+  "lat": 53.3364343,
+  "lon": -6.2487514,
+  "tags": {
+    "addr:housenumber": "114",
+    "amenity": "doctors",
+    "name": "Doctors Walk In Clinic",
+    "opening_hours": "Mo-Fr 1200-1400"
+  }
+},
+{
+  "type": "node",
+  "id": 1882359497,
+  "lat": 53.4452664,
+  "lon": -6.2248529,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "yes",
+    "name": "VHI Swiftcare Clinic"
+  }
+},
+{
+  "type": "node",
+  "id": 1977801375,
+  "lat": 53.2909223,
+  "lon": -6.3780951,
+  "tags": {
+    "amenity": "hospital",
+    "emergency": "yes",
+    "name": "National Childrens Hospital"
+  }
+}
+
+  ]
+}
